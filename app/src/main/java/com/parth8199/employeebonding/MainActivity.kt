5b60c384a3cefff54b48fc6c5b7a19c66f4d16af
parthjpatel99 <parth8199@gmail.com>
2022-04-22 17:23:12 -0400

    Added Log out in FAB Speed Dial

--- app/src/main/java/com/parth8199/employeebonding/MainActivity.kt
+++ app/src/main/java/com/parth8199/employeebonding/MainActivity.kt
@@ -1,16 +1,50 @@
 package com.parth8199.employeebonding
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.core.content.res.ResourcesCompat
+import com.leinardi.android.speeddial.SpeedDialActionItem
+import com.leinardi.android.speeddial.SpeedDialView
 import com.parse.ParseUser
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val speedDialView = findViewById<SpeedDialView>(R.id.speedDial)
+        speedDialView.addActionItem(
+            SpeedDialActionItem.Builder(R.id.fab_no_label, R.drawable.ic_baseline_exit_to_app_24_white)
+                .setFabBackgroundColor(ResourcesCompat.getColor(getResources(), R.color.purple_200, getTheme()))
+                .setFabImageTintColor(ResourcesCompat.getColor(getResources(), R.color.white, getTheme()))
+                .setLabelColor(Color.DKGRAY)
+                .setLabelBackgroundColor(ResourcesCompat.getColor(getResources(), R.color.white, getTheme()))
+                .setLabelClickable(false)
+                .setLabel("Log Out")
+                .create())
+
+        speedDialView.setOnActionSelectedListener(SpeedDialView.OnActionSelectedListener { actionItem ->
+            when (actionItem.id) {
+                R.id.fab_no_label -> {
+                    ParseUser.logOut()
+                    val currentUser = ParseUser.getCurrentUser()
+                    if (currentUser == null) {
+                        val intent = Intent(this@MainActivity, LoginActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    }
+                    Toast.makeText(this,"Logged Out", Toast.LENGTH_SHORT).show()
+                    speedDialView.close() // To close the Speed Dial with animation
+                    return@OnActionSelectedListener true // false will close it without animation
+                }
+            }
+            false
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
